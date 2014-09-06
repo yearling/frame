96d88539f006d418f6db93521757ebaf15370694
yyCom <yearlingwes@gamil.com>
2014-09-06 15:13:40 +0800

    delete YYUTRes.*

--- YYUT/YYUTres.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef __YYUTRES__
-#define __YYUTRES__
-namespace YYUT
-{
-	void YYUTCreateGUITextureFromInternalArray(CComPtr<IDirect3DDevice9> d3d_device,IDirect3DTexture9**texture,D3DXIMAGE_INFO* info);
-}
-
-#endif
